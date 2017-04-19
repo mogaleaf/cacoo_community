aa11c052b569630181db2b1b688c5ca73487f444
Cecile <cecile.gomes@mogaleaf.com>
2017-04-19 14:09:14 -0400

    Move RedisServer in main

--- src/main/java/com/mogaleaf/Main.java
+++ src/main/java/com/mogaleaf/Main.java
@@ -1,5 +1,6 @@
 package com.mogaleaf;
 
+import com.mogaleaf.community.db.impl.RedisServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        new RedisServer().start();
         SpringApplication.run(Main.class, args);
     }
 }
