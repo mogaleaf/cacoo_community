d5aa199c04fdef4e36ddf771b6f15c31e53e333a
Cecile <cecile.gomes@mogaleaf.com>
2017-04-19 23:02:19 -0400

    Unused import

--- src/main/java/com/mogaleaf/cacoo/api/DiagramsBuilder.java
+++ src/main/java/com/mogaleaf/cacoo/api/DiagramsBuilder.java
@@ -7,7 +7,6 @@ import com.mogaleaf.auth.ExecuteRequest;
 import com.mogaleaf.cacoo.model.Diagrams;
 import org.apache.http.client.utils.URIBuilder;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
